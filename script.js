560c7c76f86bd97272c8fa0f08d159e02198c564
Ajinkya Deshpande <55014143+Ajinkya2000@users.noreply.github.com>
2019-12-25 13:48:01 +0530

    Responsive Header Section

--- script.js
+++ script.js
@@ -7,29 +7,20 @@ var parentVideo = document.querySelector('.video-wrapper');
 window.addEventListener("scroll", function(){
     if (window.pageYOffset !== 0){
         nav.classList.remove('bg-transparent');
-        nav.classList.add('bg-white');
+        nav.classList.add('bg-white', 'shadow');
 
         var chngColor = document.querySelectorAll(".nav-link");
 
         for (var i=0; i < chngColor.length; i++){
             chngColor[i].classList.add("color-scroll");
         }
-        document.querySelector(".navbar-brand").style.color= "#000";
+        document.querySelector(".brand").classList.add('brand-scroll');
 
-        var hamburger = document.querySelector(".navbar-toggler");
-        hamburger.classList.add("nav-toggler-scroll");
-        hamburger.classList.remove("navbar-toggler-without-scroll");
-
-        var hamIcons = document.querySelectorAll(".ham-icons");
-        for (var i=0; i < hamIcons.length; i++){
-            hamIcons[i].classList.add('ham-scroll');
-        }
-
-        document.querySelector(".home").style.borderBottom = "2px solid red";
+        document.querySelector(".item-1").style.borderBottom = "2px solid red";
     }
 
     else {
-        nav.classList.remove('bg-white');
+        nav.classList.remove('bg-white', 'shadow');
         nav.classList.add('bg-transparent');
 
         var chngColor = document.querySelectorAll(".nav-link");
@@ -37,18 +28,26 @@ window.addEventListener("scroll", function(){
         for (var i=0; i < chngColor.length; i++){
             chngColor[i].classList.remove("color-scroll");
         }
-        document.querySelector(".navbar-brand").style.color= "#fff";
+        document.querySelector(".brand").classList.remove('brand-scroll');
 
-        var hamburger = document.querySelector(".navbar-toggler");
-        hamburger.classList.remove("nav-toggler-scroll");
-        hamburger.classList.add("navbar-toggler-without-scroll");
+        document.querySelector(".item-1").style.borderBottom = "2px solid #fff";
+    }
+});
 
-        var hamIcons = document.querySelectorAll(".ham-icons");
-        for (var i=0; i < hamIcons.length; i++){
-            hamIcons[i].classList.remove('ham-scroll');
-        }
-
-        document.querySelector(".home").style.borderBottom = "2px solid #fff";
+var a=1;
+var hamburger = document.querySelector(".ham");
+hamburger.addEventListener('click', function(){
+    document.querySelector(".ham-overlay").classList.toggle('ham-overlay--click');
+    if (document.querySelector(".ham-overlay").classList.contains('ham-overlay--click')){
+        document.querySelector(".brand").classList.add('color-hamClick');
+        document.querySelectorAll(".ham section")[0].style.background = "#000";
+        document.querySelectorAll(".ham section")[1].style.background = "#000";
+        document.querySelectorAll(".ham section")[2].style.background = "#000";
+    } else {
+        document.querySelector(".brand").classList.remove('color-hamClick');
+        document.querySelectorAll(".ham section")[0].style.background = "#fff";
+        document.querySelectorAll(".ham section")[1].style.background = "#fff";
+        document.querySelectorAll(".ham section")[2].style.background = "#fff";
     }
 });
 
@@ -58,6 +57,9 @@ function overlaySize(){
     overlay.style.width = vid.offsetWidth + 'px';
     parentVideo.style.height = vid.offsetHeight + 'px';
     parentVideo.style.width = vid.offsetWidth + 'px';
+    if (a==20){
+        document.querySelector(".brand").classList.remove('color-hamClick');
+    }
 }
 
 function leftContactLoad(){
